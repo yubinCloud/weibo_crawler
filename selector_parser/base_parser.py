3732ef5b5c78bec684b4b25631d2e9d4150b8909
yubinCloud <yubin_SkyWalker@yeah.net>
2020-08-31 16:49:54 +0800

    修补了BaseParser在__init__中接收非HTTPResponse时会报错的bug

--- selector_parser/base_parser.py
+++ selector_parser/base_parser.py
@@ -1,8 +1,13 @@
 from lxml import etree
+from tornado.httpclient import HTTPResponse
+
 
 class BaseParser:
     def __init__(self, response):
         """保存response于属性中，同时将response转化成selector"""
         self.response = response
-        self.selector = etree.HTML(self.response.body)
+        if isinstance(response, HTTPResponse):
+            self.selector = etree.HTML(self.response.body)
+        else:
+            self.selector = None
 
